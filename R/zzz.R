731bae8b6f7265c6e3e796eaa8be44dc804eb1e9
Jon Harmon <jonthegeek@gmail.com>
2023-09-14 17:16:27 -0500

    Enable S7 pre-4.3.
    This won't be needed soon, but I want to make my checks happy until that fix is merged.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 .onLoad <- function(...) {
   S7::methods_register() # nocov
 }
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
