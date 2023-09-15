ac70c2ed1666581e8a6a53d6aa9983e970f93b80
Jon Harmon <jonthegeek@gmail.com>
2023-09-14 19:14:39 -0500

    Make pkg_agent() play nice. (#27)
    Closes #26.

---
+++ R/generate_pkg_agent.R
@@ -0,0 +1,26 @@
+#' Create a user agent for the active package
+#'
+#' @param path The path to the DESCRIPTION file, or to a directory within a
+#'   package.
+#'
+#' @return A string with the name of the package and (if available) the first
+#'   URL associated with the package.
+#'
+#' @export
+generate_pkg_agent <- function(path = ".") {
+  if (!fs::is_dir(path) && fs::path_file(path) != "DESCRIPTION") {
+    path <- fs::path_dir(path) # nocov
+  }
+  pkg_desc <- desc::desc(file = path)
+  pkg_name <- pkg_desc$get_field("Package")
+  pkg_url_glue <- ""
+  pkg_url <- pkg_desc$get_urls()
+  if (length(pkg_url)) {
+    pkg_url_glue <- glue::glue(
+      " ({pkg_url[[1]]})"
+    )
+  }
+  return(
+    glue::glue("{pkg_name}{pkg_url_glue}")
+  )
+}
