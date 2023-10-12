c9ef43a4337eb9200a919513321665555e7b9a72
Jon Harmon <jonthegeek@gmail.com>
2023-10-11 19:28:54 -0500

    Don't need as_rapid(), oops!

--- tests/testthat/_fixtures/000-create_fixtures.R
+++ tests/testthat/_fixtures/000-create_fixtures.R
@@ -2,42 +2,39 @@ apid_url <- "https://api.apis.guru/v2/specs/apis.guru/2.2.0/openapi.yaml"
 api_abbr <- "guru"
 rapid_write_path <- test_path(glue::glue("_fixtures/{api_abbr}_rapid.rds"))
 config_path <- test_path(glue::glue("_fixtures/{api_abbr}_beekeeper.yml"))
-rapid <- apid_url |>
+apid_url |>
   url() |>
-  rapid::as_rapid()
-use_beekeeper(
-  rapid,
-  api_abbr = api_abbr,
-  config_file = config_path,
-  rapid_file = rapid_write_path
-)
+  use_beekeeper(
+    rapid,
+    api_abbr = api_abbr,
+    config_file = config_path,
+    rapid_file = rapid_write_path
+  )
 
 apid_url <- "https://api.apis.guru/v2/specs/fec.gov/1.0/openapi.yaml"
 api_abbr <- "fec"
 rapid_write_path <- test_path(glue::glue("_fixtures/{api_abbr}_rapid.rds"))
 config_path <- test_path(glue::glue("_fixtures/{api_abbr}_beekeeper.yml"))
-rapid <- apid_url |>
+apid_url |>
   url() |>
-  rapid::as_rapid()
-use_beekeeper(
-  rapid,
-  api_abbr = api_abbr,
-  config_file = config_path,
-  rapid_file = rapid_write_path
-)
+  use_beekeeper(
+    rapid,
+    api_abbr = api_abbr,
+    config_file = config_path,
+    rapid_file = rapid_write_path
+  )
 
 apid_url <- "https://api.apis.guru/v2/specs/trello.com/1.0/openapi.yaml"
 api_abbr <- "trello"
 rapid_write_path <- test_path(glue::glue("_fixtures/{api_abbr}_rapid.rds"))
 config_path <- test_path(glue::glue("_fixtures/{api_abbr}_beekeeper.yml"))
-rapid <- apid_url |>
+apid_url |>
   url() |>
-  rapid::as_rapid()
-use_beekeeper(
-  rapid,
-  api_abbr = api_abbr,
-  config_file = config_path,
-  rapid_file = rapid_write_path
-)
+  use_beekeeper(
+    rapid,
+    api_abbr = api_abbr,
+    config_file = config_path,
+    rapid_file = rapid_write_path
+  )
 
 warning("Revert .Rbuildignore")
