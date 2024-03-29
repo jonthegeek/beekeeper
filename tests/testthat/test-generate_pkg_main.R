b5d6f88c6e101e44ece1eef5d46acda5c4a72b2d
Jon Harmon <jonthegeek@gmail.com>
2024-03-29 10:20:37 -0500

    Scrub temp paths. (#45)
    GHA workflow runners had some confusion in test results.

--- tests/testthat/test-generate_pkg_main.R
+++ tests/testthat/test-generate_pkg_main.R
@@ -8,15 +8,13 @@ test_that("generate_pkg() returns a vector of created files", {
   saveRDS(guru_rapid, "guru_rapid.rds")
 
   test_result <- generate_pkg(pkg_agent = "TESTPKG (https://example.com)")
-  expected_result <- paste0(
-    test_dir,
-    c(
-      "/R/010-call.R",
-      "/tests/testthat/test-010-call.R",
-      "/R/paths-apis.R",
-      "/tests/testthat/test-paths-apis.R",
-      "/tests/testthat/setup.R"
-    )
+  test_result <- scrub_path(test_result)
+  expected_result <- c(
+    "/R/010-call.R",
+    "/tests/testthat/test-010-call.R",
+    "/R/paths-apis.R",
+    "/tests/testthat/test-paths-apis.R",
+    "/tests/testthat/setup.R"
   )
 
   expect_identical(test_result, expected_result)
