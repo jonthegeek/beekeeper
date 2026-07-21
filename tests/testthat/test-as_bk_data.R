535e1cf6118e65f4c2d93c9699bff99728f2d9cd
Jon Harmon <jonthegeek@gmail.com>
2026-07-21 08:53:54 -0500

    Use stbl update

--- tests/testthat/test-as_bk_data.R
+++ tests/testthat/test-as_bk_data.R
@@ -7,9 +7,6 @@ test_that("as_bk_data warns for unknown classes", {
     "bk_data",
     "missing_method"
   )
-
-  # Workaround for wranglezone/stbl#234
-  test_result <- suppressWarnings(as_bk_data("a"))
   expect_type(test_result, "list")
   expect_length(test_result, 0)
 })
